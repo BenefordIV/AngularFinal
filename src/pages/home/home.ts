a7db3725e2f125736d1fa7e7dd75050381c0e6be
Ben Steier <bensteier713@gmail.com>
2018-03-20 12:58:23 -0500

    First message, read description of repo

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AnimeProvider } from '../../providers/anime/anime';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,10 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+anime:any;
 
+  constructor(public navCtrl: NavController, public animeProvider:AnimeProvider) {
+    animeProvider.getData().subscribe(data => this.anime = JSON.stringify(data));
   }
 
 }
